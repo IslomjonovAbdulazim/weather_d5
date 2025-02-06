1945da838100bd497391d0dbcc78e60e8d98f521
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-06 17:07:15 +0500

    device

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(WeatherApp());
+  runApp(DevicePreview(builder: (_) => WeatherApp()));
 }
 
 class WeatherApp extends StatelessWidget {
